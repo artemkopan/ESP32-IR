69d645e5b8ba34815da7349aa3c665b717218f0a
artem kopan <artem.kopan@poynt.co>
2021-05-25 20:34:09 +0300

    add parsing mqtt json data

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,58 @@
 #include "WiFi.h" // ESP32 WiFi include
 #include <PubSubClient.h>
 #include <Wire.h>
-
 #include "esp_log.h"
+#include <ArduinoJson.h>
 
-#define RECEIVER_PIN 5
+#define RECEIVER_PIN 26
 #define SENDER_PIN 4
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#define mqtt_server "broker.emqx.io"
-#define ir_sending_topic "artem_k_ir_sending_topic"
+#define mqtt_server "192.168.1.77"
+#define mqtt_username "admin"
+#define mqtt_pass "274733"
+#define ir_sending_topic "ir_sending_topic"
+
+const char *type_rc6 = "rc6";
+const char *type_nec = "nec";
 
 void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrived in topic: ");
     Serial.println(topic);
 
-    Serial.print("Message:");
+    char message[length];
+    Serial.print("Message: ");
+
     for (int i = 0; i < length; i++) {
         Serial.print((char) payload[i]);
+        message[i] = (char) payload[i];
     }
 
+    StaticJsonDocument<256> doc;
+    deserializeJson(doc, message);
+
+    const char *type = doc["type"];
+
+    uint8_t address = doc["address"];
+    uint8_t command = doc["command"];
+    uint_fast8_t repeats = doc["repeats"];
+    bool automaticToggle = doc["automaticToggle"];
+
+    Serial.println();
+    Serial.print("Json parsed");
+
+    if (strcmp(type, type_rc6) == 0) {
+        Serial.print("Send RC6");
+        IrSender.sendRC6(address, command, repeats, automaticToggle);
+    } else if (strcmp(type, type_nec) == 0) {
+        Serial.print("Send NEC");
+        IrSender.sendNEC(address, command, repeats, automaticToggle);
+    } else {
+        Serial.print("Unknown type: ");
+        Serial.print(type);
+    }
     Serial.println();
     Serial.println("-----------------------");
 }
@@ -35,35 +66,20 @@ void setup_wifi() {
     delay(100);
     WiFiClass::mode(WIFI_STA);
 
-    // Set your Static IP address
-    IPAddress local_IP(192, 168, 1, 184);
-// Set your Gateway IP address
-    IPAddress gateway(192, 168, 1, 1);
-    IPAddress subnet(255, 255, 0, 0);
-
-    // Configures static IP address
-    if (!WiFi.config(local_IP, gateway, subnet)) {
-        Serial.println("STA Failed to configure");
-    }
-
     WiFi.begin(ssid, pass);
     WiFi.setAutoConnect(true);
     WiFi.setAutoReconnect(true);
 
     while (WiFiClass::status() != WL_CONNECTED) {
         delay(500);
-//        Serial.print("Wifi status: ");
-//        Serial.print(WiFiClass::status());
     }
-    Serial.println("Wifi status:");
-    Serial.print(WiFiClass::status());
 }
 
 void reconnect() {
     // Loop until we're reconnected
     while (!client.connected()) {
         Serial.print("Attempting MQTT connection...");
-        if (client.connect("esp32Ir")) {
+        if (client.connect("esp32Ir", mqtt_username, mqtt_pass)) {
             Serial.println("connected");
             client.setCallback(callback);
             client.subscribe(ir_sending_topic);
@@ -85,40 +101,18 @@ void setup() {
     esp_log_level_set("wifi", ESP_LOG_DEBUG);      // enable WARN logs from WiFi stack
     esp_log_level_set("dhcpc", ESP_LOG_INFO);     // enable INFO logs from DHCP client
 
-//    serialBT.begin("ESP32"); // Может придумать своё имя
-
     setup_wifi();
-
-//    client.setServer(mqtt_server, 1883);
+    client.setServer(mqtt_server, 1883);
 }
 
 void loop() {
-//    if (IrReceiver.decode()) {
-//        IrReceiver.printIRResultShort(&Serial);
-//        IrReceiver.resume(); // Continue receiving
-//    }
-//    IrSender.sendNEC(0xE78,)
-
-//    if (serialBT.available()) {
-//        char incomingChar = serialBT.read();
-//        Serial.write(incomingChar);
-//
-//        // При символе "1" включаем светодиод
-//        if (incomingChar == '1')
-//        {
-//            Serial.println("On");
-//        }
-//        // При символе "0" выключаем светодиод
-//        if ( incomingChar == '0')
-//        {
-//            Serial.println("Off");
-//        }
-//    }
-
-//    if (!client.connected()) {
-//        reconnect();
-//    }
-//    client.loop();
-
-    delay(1000);
+    if (IrReceiver.decode()) {
+        IrReceiver.printIRResultShort(&Serial);
+        IrReceiver.resume(); // Continue receiving
+    }
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+    delay(10);
 }
